162eabc16779fb1266dd63e4e3e11d956e531e7c
Liu Yamin <rakeliu@hotmail.com>
2021-05-18 16:12:19 +0800

    split

--- k8s/installation/sed_var.sh
+++ k8s/installation/sed_var.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+function _INIT_SED_VAR()
+{
+  # docker-hub setting
+  SED_VAR["DOCKER_HUB"]="docker-hub:5000"
+  # nfs setting
+  SED_VAR["NFS_SERVER"]="192.168.176.8"
+  SED_VAR["NFS_PATH"]="/appdata/nfs"
+  # docker setting
+  SED_VAR["DOCKER_DATA_ROOT"]="/appdata/docker"
+  # coredns setting
+  SED_VAR["INNER_DNS"]="10.15.0.2"
+  SED_VAR["OUTTER_DNS"]="172.18.0.4"
+}
+
 function _GET_DIR()
 {
   if [ $# -eq 0 ]; then
@@ -30,13 +44,7 @@ declare -ra DIRS=($(_GET_DIR $@))
 
 # declare map
 declare -A SED_VAR
-# docker-hub setting
-SED_VAR["DOCKER_HUB"]="docker-hub:5000"
-# nfs setting
-SED_VAR["NFS_SERVER"]="192.168.176.8"
-SED_VAR["NFS_PATH"]="/appdata/nfs"
-# docker setting
-SED_VAR["DOCKER_DATA_ROOT"]="/appdata/docker"
+_INIT_SED_VAR
 
 # -------- main -----------
 _sed_in_map ${DIRS[@]}
